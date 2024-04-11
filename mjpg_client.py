bcf6e94ff7da3c738aa4568e5b6f7679b985d44c
Dan Napert <dannapert@gmail.com>
2024-04-11 08:20:30 -0400

    Revert " exceptions"
    This reverts commit 1f9c8f6682167da0123b730d09d13041ff602cc2.

--- mjpg_client.py
+++ mjpg_client.py
@@ -1,17 +1,9 @@
 import cv2
-from ultralytics import YOLO
-
-model = YOLO('yolov8n.pt')
 
 stream = cv2.VideoCapture('http://192.168.0.183:5000/video')
 while True:
-    try:
-        ret,frame = stream.read()
-    except:
-        pass
-    if ret:
-        img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = model.track(img_rgb,conf=.5,verbose=False,device=1,save=False,show=True)
-        if cv2.waitKey(1) == 27:
-            break
-stream.release()
+    ret,frame = stream.read()
+    img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    cv2.imshow('Client',img_rgb)
+    if cv2.waitKey(1) == 27:
+        break
