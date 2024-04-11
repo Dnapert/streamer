5a952612cc05be1eca2631d882f8621a37778527
Dan Napert <dannapert@gmail.com>
2024-04-11 08:26:17 -0400

    logic

--- mjpg_client.py
+++ mjpg_client.py
@@ -4,10 +4,10 @@ stream = cv2.VideoCapture('http://192.168.0.183:5000/video')
 while True:
     try:
         ret,frame = stream.read()
-        if ret:
-            img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            cv2.imshow('Client',img_rgb)
-            if cv2.waitKey(1) == 27:
-                break
     except:
         pass
+    if ret:
+        img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        cv2.imshow('Client',img_rgb)
+        if cv2.waitKey(1) == 27:
+            break
