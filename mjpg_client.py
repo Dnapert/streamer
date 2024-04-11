a506d58f7d5b3988591ff8b9d47f5eabd70a5c26
Dan Napert <dannapert@gmail.com>
2024-04-11 08:30:49 -0400

    forget the exceptions

--- mjpg_client.py
+++ mjpg_client.py
@@ -2,10 +2,7 @@ import cv2
 
 stream = cv2.VideoCapture('http://192.168.0.183:5000/video')
 while True:
-    try:
-        ret,frame = stream.read()
-    except:
-        pass
+    ret,frame = stream.read()
     if ret:
         img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         cv2.imshow('Client',img_rgb)
